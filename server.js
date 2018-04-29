2b6214f38a34cfc01049192852d844985e1518c2
karthikhacker <karthik.hacker85@gmail.com>
2018-04-29 08:54:20 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+//app.use(express.static(path.join(__dirname + '/public')));
+
+//routes
+app.get('/api/whoami',(req,res) => {
+
+  var lang = req.acceptsLanguages()
+  var ip = req.ip;
+  var os = req.get('user-agent');
+  res.json({"Language" : lang[0],"operating system" : os,  'IP' : ip});
+});
+//server
+app.listen(port,() => {
+  console.log('App running at port',port);
+});
